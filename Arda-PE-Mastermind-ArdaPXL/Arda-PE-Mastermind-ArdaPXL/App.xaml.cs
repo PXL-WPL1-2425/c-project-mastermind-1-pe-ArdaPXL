bc0d89b2df32b9c749e07091cd94011dcba0479d
ArdaPXL <12401647@student.pxl.be>
2024-11-18 14:37:39 +0100

    Mastermind-PE1-01 Pogingen Zorg ervoor dat de speler meerdere pogingen kan doen om de code te kraken. Toon "Poging x" in de Windows titel. Voorzie een variabele attempts hiervoor.

---
+++ Arda-PE-Mastermind-ArdaPXL/Arda-PE-Mastermind-ArdaPXL/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Arda_PE_Mastermind_ArdaPXL
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Arda-PE-Mastermind-ArdaPXL/Arda-PE-Mastermind-ArdaPXL/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mastermind
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> colors = new List<string> { "Red", "Yellow", "Orange", "White", "Green", "Blue" };
+        private List<string> code;
+        //private List<string> attempts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGame();
+
+        }
+
+        private void InitializeGame()
+        {
+            // Generate a random code
+            Random rand = new Random();
+            code = new List<string>();
+            //attempts = new List<string>();
+            for (int i = 0; i < 4; i++)
+            {
+                //code.Add(attempts[rnd.Next(attempts.Count)]);
+                code.Add(colors[rand.Next(colors.Count)]);
+            }
+            int attempts = rand.Next(0, 5);
+            this.Title = $"Mastermind - Code: {string.Join(", ", code)} -- Pogingen: {attempts}";
+            //"Mastermind - Code: " + string.Join(", ", code) + "pogingen:" + poging;
+
+
+
+
+            // Populate ComboBoxes with color options
+            ComboBox[] comboBoxes = { ComboBox1, ComboBox2, ComboBox3, ComboBox4 };
+            for (int i = 0; i < comboBoxes.Length; i++)
+            {
+                comboBoxes[i].ItemsSource = colors;
+            }
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            System.Windows.Controls.Label label = null;
+
+            if (comboBox == ComboBox1) label = Label1;
+            else if (comboBox == ComboBox2) label = Label2;
+            else if (comboBox == ComboBox3) label = Label3;
+            else if (comboBox == ComboBox4) label = Label4;
+
+            if (label != null && comboBox.SelectedItem != null)
+            {
+                string selectedColor = comboBox.SelectedItem.ToString();
+                label.Content = selectedColor;
+                label.Background = GetColorBrush(selectedColor);
+                label.BorderBrush = Brushes.Transparent; // Reset border color
+            }
+        }
+        private Brush GetColorBrush(string colorName)
+        {
+            if (colorName == "Red") return Brushes.Red;
+            if (colorName == "Yellow") return Brushes.Yellow;
+            if (colorName == "Orange") return Brushes.Orange;
+            if (colorName == "White") return Brushes.White;
+            if (colorName == "Green") return Brushes.Green;
+            if (colorName == "Blue") return Brushes.Blue;
+            return Brushes.Transparent;
+        }
+        private void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBox[] comboBoxes = { ComboBox1, ComboBox2, ComboBox3, ComboBox4 };
+            List<string> selectedColors = new List<string>();
+
+            for (int i = 0; i < comboBoxes.Length; i++)
+            {
+                if (comboBoxes[i].SelectedItem != null)
+                {
+                    selectedColors.Add(comboBoxes[i].SelectedItem.ToString());
+                }
+                else
+                {
+                    selectedColors.Add(null);
+                }
+            }
+            System.Windows.Controls.Label[] labels = { Label1, Label2, Label3, Label4 };
+            for (int i = 0; i < labels.Length; i++)
+            {
+                if (selectedColors[i] == code[i])
+                {
+                    labels[i].BorderBrush = Brushes.Green; // Correct color and position
+                }
+                else if (code.Contains(selectedColors[i]))
+                {
+                    labels[i].BorderBrush = Brushes.Orange; // Correct color, wrong position
+                }
+                else
+                {
+                    labels[i].BorderBrush = Brushes.DarkRed; // Incorrect color
+                }
+            }
+        }
+
+
+
+    }
+}
